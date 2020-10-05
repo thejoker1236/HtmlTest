ac9e49bc82a96a8529159321786f4e87d5308a0d
meshan.fernando@syrocon.de <meshan.fernando@syrocon.de>
2020-10-05 13:45:11 +0200

    Initial commit

---
+++ SuperDuperWebsite/resources/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.js--section--feature').waypoint(function(direction) {
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        } 
+    }, {
+        offset: '40px'
+    })
+});
